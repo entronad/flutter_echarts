ebb5e9424dce7a6ce6842d4f9fc18ee42741c479
goldenpanda_ <akramaskar@gmail.com>
2021-04-25 08:36:18 +0200

    Add reloadAfterInit

--- lib/flutter_echarts.dart
+++ lib/flutter_echarts.dart
@@ -14,21 +14,23 @@ import 'echarts_script.dart' show echartsScript;
 
 /// <!DOCTYPE html><html><head><meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=0, target-densitydpi=device-dpi" /><style type="text/css">body,html,#chart{height: 100%;width: 100%;margin: 0px;}div {-webkit-tap-highlight-color:rgba(255,255,255,0);}</style></head><body><div id="chart" /></body></html>
 /// 'data:text/html;base64,' + base64Encode(const Utf8Encoder().convert( /* STRING ABOVE */ ))
-const htmlBase64 = 'data:text/html;base64,PCFET0NUWVBFIGh0bWw+PGh0bWw+PGhlYWQ+PG1ldGEgY2hhcnNldD0idXRmLTgiPjxtZXRhIG5hbWU9InZpZXdwb3J0IiBjb250ZW50PSJ3aWR0aD1kZXZpY2Utd2lkdGgsIGluaXRpYWwtc2NhbGU9MS4wLCBtYXhpbXVtLXNjYWxlPTEuMCwgbWluaW11bS1zY2FsZT0xLjAsIHVzZXItc2NhbGFibGU9MCwgdGFyZ2V0LWRlbnNpdHlkcGk9ZGV2aWNlLWRwaSIgLz48c3R5bGUgdHlwZT0idGV4dC9jc3MiPmJvZHksaHRtbCwjY2hhcnR7aGVpZ2h0OiAxMDAlO3dpZHRoOiAxMDAlO21hcmdpbjogMHB4O31kaXYgey13ZWJraXQtdGFwLWhpZ2hsaWdodC1jb2xvcjpyZ2JhKDI1NSwyNTUsMjU1LDApO308L3N0eWxlPjwvaGVhZD48Ym9keT48ZGl2IGlkPSJjaGFydCIgLz48L2JvZHk+PC9odG1sPg==';
+const htmlBase64 =
+    'data:text/html;base64,PCFET0NUWVBFIGh0bWw+PGh0bWw+PGhlYWQ+PG1ldGEgY2hhcnNldD0idXRmLTgiPjxtZXRhIG5hbWU9InZpZXdwb3J0IiBjb250ZW50PSJ3aWR0aD1kZXZpY2Utd2lkdGgsIGluaXRpYWwtc2NhbGU9MS4wLCBtYXhpbXVtLXNjYWxlPTEuMCwgbWluaW11bS1zY2FsZT0xLjAsIHVzZXItc2NhbGFibGU9MCwgdGFyZ2V0LWRlbnNpdHlkcGk9ZGV2aWNlLWRwaSIgLz48c3R5bGUgdHlwZT0idGV4dC9jc3MiPmJvZHksaHRtbCwjY2hhcnR7aGVpZ2h0OiAxMDAlO3dpZHRoOiAxMDAlO21hcmdpbjogMHB4O31kaXYgey13ZWJraXQtdGFwLWhpZ2hsaWdodC1jb2xvcjpyZ2JhKDI1NSwyNTUsMjU1LDApO308L3N0eWxlPjwvaGVhZD48Ym9keT48ZGl2IGlkPSJjaGFydCIgLz48L2JvZHk+PC9odG1sPg==';
 
 class Echarts extends StatefulWidget {
-  Echarts({
-    Key key,
-    @required this.option,
-    this.extraScript = '',
-    this.onMessage,
-    this.extensions = const [],
-    this.theme,
-    this.captureAllGestures = false,
-    this.captureHorizontalGestures = false,
-    this.captureVerticalGestures = false,
-    this.onLoad,
-  }) : super(key: key);
+  Echarts(
+      {Key key,
+      @required this.option,
+      this.extraScript = '',
+      this.onMessage,
+      this.extensions = const [],
+      this.theme,
+      this.captureAllGestures = false,
+      this.captureHorizontalGestures = false,
+      this.captureVerticalGestures = false,
+      this.onLoad,
+      this.reloadAfterInit = false})
+      : super(key: key);
 
   final String option;
 
@@ -48,6 +50,8 @@ class Echarts extends StatefulWidget {
 
   final void Function() onLoad;
 
+  final bool reloadAfterInit;
+
   @override
   _EchartsState createState() => _EchartsState();
 }
@@ -59,21 +63,30 @@ class _EchartsState extends State<Echarts> {
 
   // --- FIX_BLINK ---
   double _opacity = Platform.isAndroid ? 0.0 : 1.0;
+
   // --- FIX_BLINK ---
 
   @override
   void initState() {
     super.initState();
     _currentOption = widget.option;
+
+    if (widget.reloadAfterInit) {
+      new Future.delayed(const Duration(milliseconds: 100), () {
+        _controller.reload();
+      });
+    }
   }
 
   void init() async {
     final extensionsStr = this.widget.extensions.length > 0
-    ? this.widget.extensions.reduce(
-        (value, element) => (value ?? '') + '\n' + (element ?? '')
-      )
-    : '';
-    final themeStr = this.widget.theme != null ? '\'${this.widget.theme}\'' : 'null';
+        ? this
+            .widget
+            .extensions
+            .reduce((value, element) => (value ?? '') + '\n' + (element ?? ''))
+        : '';
+    final themeStr =
+        this.widget.theme != null ? '\'${this.widget.theme}\'' : 'null';
     await _controller?.evaluateJavascript('''
       $echartsScript
       $extensionsStr
@@ -88,7 +101,8 @@ class _EchartsState extends State<Echarts> {
 
   Set<Factory<OneSequenceGestureRecognizer>> getGestureRecognizers() {
     Set<Factory<OneSequenceGestureRecognizer>> set = Set();
-    if(this.widget.captureAllGestures || this.widget.captureHorizontalGestures) {
+    if (this.widget.captureAllGestures ||
+        this.widget.captureHorizontalGestures) {
       set.add(Factory<HorizontalDragGestureRecognizer>(() {
         return HorizontalDragGestureRecognizer()
           ..onStart = (DragStartDetails details) {}
@@ -98,7 +112,7 @@ class _EchartsState extends State<Echarts> {
           ..onEnd = (DragEndDetails details) {};
       }));
     }
-    if(this.widget.captureAllGestures || this.widget.captureVerticalGestures) {
+    if (this.widget.captureAllGestures || this.widget.captureVerticalGestures) {
       set.add(Factory<VerticalDragGestureRecognizer>(() {
         return VerticalDragGestureRecognizer()
           ..onStart = (DragStartDetails details) {}
@@ -137,38 +151,38 @@ class _EchartsState extends State<Echarts> {
     }
     super.dispose();
   }
+
   // --- FIX_IOS_LEAK ---
 
   @override
   Widget build(BuildContext context) {
     // --- FIX_BLINK ---
     return Opacity(
-      opacity: _opacity,
-    // --- FIX_BLINK ---
-      child: WebView(
-        initialUrl: htmlBase64,
-        javascriptMode: JavascriptMode.unrestricted,
-        onWebViewCreated: (WebViewController webViewController) {
-          _controller = webViewController;
-        },
-        onPageFinished: (String url) {
-          // --- FIX_BLINK ---
-          if (Platform.isAndroid) {
-            setState(() { _opacity = 1.0; });
-          }
-          // --- FIX_BLINK ---
-          init();
-        },
-        javascriptChannels: <JavascriptChannel>[
-          JavascriptChannel(
-            name: 'Messager',
-            onMessageReceived: (JavascriptMessage javascriptMessage) {
-              widget?.onMessage(javascriptMessage.message);
-            }
-          ),
-        ].toSet(),
-        gestureRecognizers: getGestureRecognizers()
-      )
-    );
+        opacity: _opacity,
+        // --- FIX_BLINK ---
+        child: WebView(
+            initialUrl: htmlBase64,
+            javascriptMode: JavascriptMode.unrestricted,
+            onWebViewCreated: (WebViewController webViewController) {
+              _controller = webViewController;
+            },
+            onPageFinished: (String url) {
+              // --- FIX_BLINK ---
+              if (Platform.isAndroid) {
+                setState(() {
+                  _opacity = 1.0;
+                });
+              }
+              // --- FIX_BLINK ---
+              init();
+            },
+            javascriptChannels: <JavascriptChannel>[
+              JavascriptChannel(
+                  name: 'Messager',
+                  onMessageReceived: (JavascriptMessage javascriptMessage) {
+                    widget?.onMessage(javascriptMessage.message);
+                  }),
+            ].toSet(),
+            gestureRecognizers: getGestureRecognizers()));
   }
 }
